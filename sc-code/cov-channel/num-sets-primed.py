3348de8b767cd16be99cbaeb49ed85120bf15cf8
Tarunesh Verma <tarunesh@umich.edu>
2021-04-11 12:41:10 -0400

    sweep associativity
    updated p(n) script to plot it with sweeping associativities

--- sc-code/cov-channel/num-sets-primed.py
+++ sc-code/cov-channel/num-sets-primed.py
@@ -4,36 +4,38 @@ import seaborn as sns
 import numpy as np
 
 system_random = random.SystemRandom()
-num_trials = 50
+num_trials = 20
 
 cache_size = 512*1024
-associativity = 16
-num_sets = int(cache_size/(associativity * 64))
-num_lines = int(num_sets * associativity)
+associativity = [4, 8, 12, 16, 20, 32]
 
 acc, bins, sets, results = 0, [], [], {}
-bins = [i for i in range(0, num_sets+1)]
-for i in range(num_lines):
-    results[i] = 0.0
  
-for i in range(num_trials):
-    sets = [system_random.randint(0, num_sets - 1) for k in range(num_lines)]
-    for j in range(0, num_lines, 100):
-        acc = 0
-        curr_sets = system_random.sample(sets, j)
-        hist, edges = np.histogram(curr_sets, bins)
+for assoc in associativity:
+    num_sets = int(cache_size/(assoc * 64))
+    num_lines = int(num_sets * assoc)
+    bins = [i for i in range(0, num_sets+1)]
+    for i in range(num_lines):
+        results[i] = 0.0
 
-        for k in range(len(hist)):
-            if hist[k] >= associativity:
-                acc += 1
-        value = results[j] + acc/(num_sets*num_trials)
-        results.update({j:value})
+    for trial in range(num_trials):
+        sets = [system_random.randint(0, num_sets - 1) for i in range(num_lines)]
+        for lines in range(0, num_lines):
+            acc = 0
+            curr_sets = system_random.sample(sets, lines)
+            hist, edges = np.histogram(curr_sets, bins)
 
-plt.plot(*zip(*sorted(results.items())))
+            for this_set in range(len(hist)):
+                if hist[this_set] >= assoc:
+                    acc += 1
+            value = results[lines] + acc/(num_sets*num_trials)
+            results.update({lines:value})
+
+    plt.plot(*zip(*sorted(results.items())))
+
 plt.xlabel('Number of random lines written (n)')
 plt.ylabel('Percentage of cache sets primed (p)')
-plt.title('p(n)')
+plt.title('p(n), 512kB randomized LLC, 8192 lines')
+plt.legend([str(assoc) for assoc in associativity], title='Associativity of\nrandomized cache')
 #plt.show()
 plt.savefig('priming.png', format='png', dpi=1200)
-
-
