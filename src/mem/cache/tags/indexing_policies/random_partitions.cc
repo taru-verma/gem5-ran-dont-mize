12943f1e0d3186bab1f29266c05d74d71e3a1ac5
Tarunesh Verma <tarunesh@umich.edu>
2021-09-09 16:48:49 -0400

    randomized partitions update
    Set default indexing policy back to SetAssociative, RandomPartitions can be set from cache config file through tags object; updated random_partitions.cc with changes from covert-channels branch (using NUM_PARTITIONS and some cleanup)

--- src/mem/cache/tags/indexing_policies/random_partitions.cc
+++ src/mem/cache/tags/indexing_policies/random_partitions.cc
@@ -11,6 +11,8 @@
 
 #include <math.h>
 
+#define NUM_PARTITIONS 1 
+
 RandomPartitions::RandomPartitions(const Params &p)
     : BaseIndexingPolicy(p)
 {
@@ -26,7 +28,6 @@ Addr
 RandomPartitions::regenerateAddr(const Addr tag, const Addr orig_set, const ReplaceableEntry* entry)
                                                                         const
 {
-    // disable_randomization - change `orig_set` to `entry->getSet()`
     return (tag << tagShift) | (orig_set << setShift);
 }
 
@@ -34,11 +35,11 @@ std::vector<ReplaceableEntry*>
 RandomPartitions::getPossibleEntries(const Addr addr) const
 {
     std::vector<ReplaceableEntry*> entries;
-    int i = 0, num_partitions = 4;
-    int partition_size = assoc/num_partitions;
+    int i = 0, partition_size = assoc/NUM_PARTITIONS;
 
     // Pick a partition at random and get the encrypted address for that partition
-    int random_partition = rand() % num_partitions;
+    // Don't use extractSet() since you encrypt the tag+set portion of the address
+    int random_partition = rand() % NUM_PARTITIONS;
     Addr encrypted_addr = speck_encrypt_wrapper(addr >> setShift, random_partition);
 
     // Get all ways for the set in that partition, replacement policy chooses where to put.
@@ -53,18 +54,17 @@ RandomPartitions::getAllPossibleEntries(const Addr addr) const
 {
     std::vector<ReplaceableEntry*> entries;
     std::vector<Addr> encrypted_addresses;
-    int num_partitions = 4;
     int i = 0, j = 0;
 
     // Generate encrypted addresses for all partitions since 
     // while writing partition was chosen randomly
-    for (i = 0; i < num_partitions; ++i)
+    for (i = 0; i < NUM_PARTITIONS; ++i)
         encrypted_addresses.push_back(speck_encrypt_wrapper(addr >> setShift, i));
 
     // Return all ways for the set in each partition
-    for (i = 0; i < num_partitions; ++i)
-        for (j = 0; j < assoc/num_partitions; ++j)
-            entries.push_back(sets[extractSet(encrypted_addresses[i])][j+i*assoc/num_partitions]);
+    for (i = 0; i < NUM_PARTITIONS; ++i)
+        for (j = 0; j < assoc/NUM_PARTITIONS; ++j)
+            entries.push_back(sets[extractSet(encrypted_addresses[i])][j+i*assoc/NUM_PARTITIONS]);
 
     return entries;
 }
